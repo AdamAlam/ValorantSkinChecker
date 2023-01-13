89d1ae806d5e3f6cb53f6f6db0d8284bed27296e
Adam Alam <adamalam7@outlook.com>
2023-01-12 20:38:13 -0600

    Fixed bug where file would only be read at start of server.

--- API/index.js
+++ API/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const fs = require("fs/promises");
 
 app.use(cors({ origin: "http://localhost:80" }));
 
@@ -13,13 +14,24 @@ app.use(
 );
 
 app.get("/:discordId", (req, res) => {
-  let userData = require("/root/ValorantSkinChecker/API/userSkinData.json");
-  myData = userData.filter((obj) => obj.account == req.params.discordId);
-  if (myData.length > 0) {
-    res.send(myData[0]);
-  } else {
-    res.send({});
-  }
+  myData = readData(req.params.discordId).then((myData) => {
+    if (Object.keys(myData).length > 0) {
+      res.send(myData).status(200);
+    } else {
+      res.send({}).status(200);
+    }
+  });
 });
+
+const readData = (discordId) => {
+  return fs
+    .readFile("/root/ValorantSkinChecker/API/userSkinData.json")
+    .then((data) => {
+      return JSON.parse(data).filter((obj) => obj.account == discordId).length >
+        0
+        ? JSON.parse(data).filter((obj) => obj.account == discordId)[0]
+        : [];
+    });
+};
 
 app.listen(8080, () => console.log("Server started on port 8080"));
